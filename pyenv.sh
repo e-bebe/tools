04746bd80ae757dc501549ec50a68942952a6f13
yokkora <freewave_3@hotmail.com>
2015-05-11 06:37:37 +0000

    add pyenv

---
+++ pyenv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir ~/tools/test && cd ~/tools/test
+pyenv versions
+
+#pyenv virtualenv 3.4.3 hogege
+#pyenv versions
+
+#set local virtualenv 
+#pyenv local hogege
+
+#pyenv install --list
+#pyenv install 3.4.3
