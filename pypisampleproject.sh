be49f3c0cef38c48cf02e5576acfdafe88785eb8
yokkora <freewave_3@hotmail.com>
2015-05-11 08:14:31 +0000

    add how to test project

--- pypisampleproject.sh
+++ pypisampleproject.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 git clone https://github.com/pypa/sampleproject.git
+
+# 作成したmoduleの実行方法
+
+#virtualenvになるか
+#pyenv local hogegeなどで別仮想環境になり、
+#$pip install git+https://github.com/hooor/sampleproject.git
+#
+#$python
+#>>>import sampleproject
+#>>>sampleproject.main()
+#
+#で実行可能
